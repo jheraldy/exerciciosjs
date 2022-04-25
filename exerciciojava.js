d7f6f60be9734441d7da51b8d96898499e1442e3
jheraldy <100284364+jheraldy@users.noreply.github.com>
2022-04-25 11:54:44 -0300

    Add files via upload

---
+++ exerciciojava.js
@@ -0,0 +1,11 @@
+function clicado(){
+    document.querySelector(".quadrado").innerHTML = "Fui Clicado"
+
+}
+function mouse(){
+    document.querySelector(".quadrado").innerHTML = "Passou o Mouse aqui"
+}
+
+function tirou(){
+    document.querySelector(".quadrado").innerHTML = "Tirou o Mouse"
+}
